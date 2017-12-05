27e473663c4894b3b596548fffbcaeb027261823
Jose Bejarano Carranza <jabcarranz@gmail.com>
2017-12-04 21:13:34 -0500

    Initial commit

---
+++ app/src/main/java/com/project/deb/cosmetics/Fragments/ContactanosFragment.java
@@ -0,0 +1,75 @@
+package com.project.deb.cosmetics.Fragments;
+
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.project.deb.cosmetics.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContactanosFragment extends Fragment implements OnMapReadyCallback{
+
+    MapView mapView;
+    GoogleMap googleMap;
+    View view;
+
+    public ContactanosFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view =  inflater.inflate(R.layout.fragment_contactanos, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = (MapView) view.findViewById(R.id.map);
+        if(mapView != null){
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        MapsInitializer.initialize(getContext());
+
+        LatLng ll = new LatLng(-12.122838, -77.029972);
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(ll));
+
+        CameraPosition pos = CameraPosition.builder().target(ll).zoom(16).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(pos));
+    }
+}
---
+++ app/src/main/java/com/project/deb/cosmetics/MainActivity.java
@@ -0,0 +1,105 @@
+package com.project.deb.cosmetics;
+
+
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.project.deb.cosmetics.Fragments.ContactanosFragment;
+import com.project.deb.cosmetics.Fragments.DisenosFragment;
+import com.project.deb.cosmetics.Fragments.MainFragment;
+import com.project.deb.cosmetics.Fragments.PresentacionFragment;
+import com.project.deb.cosmetics.Fragments.RedesFragment;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] drawerListViewItems;
+    private ListView drawerListView;
+    private DrawerLayout drawerlayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerListView = (ListView) findViewById(R.id.left_drawer);
+        drawerlayout = (DrawerLayout) findViewById(R.id.drwrlayout);
+
+        Fragment fragment = new MainFragment();
+        inflateFragment(fragment);
+
+        drawerListViewItems = getResources().getStringArray(R.array.Menu_entries);
+
+        drawerListView.setAdapter(new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_activated_1,
+                android.R.id.text1,
+                drawerListViewItems));
+
+        drawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                selectItem(i);
+
+            }
+        });
+
+
+        ImageView btnmenu = (ImageView) findViewById(R.id.btnMenu);
+        btnmenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerlayout.openDrawer(Gravity.START);
+            }
+        });
+
+
+    }
+
+    private void selectItem(int pos){
+
+        if(pos == 0){//presentacion
+            Fragment frag = new PresentacionFragment();
+            inflateFragment(frag);
+        }
+        if(pos == 1){//disenos
+            Fragment frag = new DisenosFragment();
+            inflateFragment(frag);
+        }
+        if(pos == 2){//contactenos
+            Fragment frag = new ContactanosFragment();
+            inflateFragment(frag);
+        }
+        if(pos == 3){//redes
+            Fragment frag = new RedesFragment();
+            inflateFragment(frag);
+        }
+    }
+
+
+    private void inflateFragment(Fragment fragment) {
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.mainfrm, fragment);
+            ft.commit();
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drwrlayout);
+            drawer.closeDrawer(GravityCompat.START);
+        }
+    }
+
+
+
+}
